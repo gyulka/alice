ffab69c28e16cf271f1a8d35d5b35a3fcab1df86
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-25 21:06:58 +0500

    run

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import logging
 
@@ -131,4 +132,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
