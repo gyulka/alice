9c158f8df193827d4f5d50591ab38698017a1957
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-26 19:08:00 +0500

    алиса угадай город на карте

--- main.py
+++ main.py
@@ -84,44 +84,167 @@ def handle_dialog(res, req):
     user_id = req['session']['user_id']
 
     if req['session']['new']:
-        res['response'][
-            'text'] = 'Привет! Я могу сказать в какой стране город или сказать расстояние между городами!'
+        res['response']['text'] = 'Привет! Назови свое имя!'
+        sessionStorage[user_id] = {
+            'first_name': None,
+            'game_started': False
+        }
 
         return
 
-    cities = get_cities(req)
+    first_name = get_first_name(req)
 
-    if len(cities) == 0:
+    if sessionStorage[user_id]['first_name'] is None:
 
-        res['response']['text'] = 'Ты не написал название не одного города!'
+        if first_name is None:
+            res['response']['text'] = 'Не раслышала имя. Повтори!'
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            sessionStorage[user_id]['guessed_cities'] = []
+            res['response']['text'] = 'Приятно познакомиться, ' + first_name.title() + '. Я Алиса. ' \
+                                                                                       'Отгадаешь город по фото?'
+            res['response']['buttons'] = [
+                {
+                    'title': 'Да',
+                    'hide': True
 
-    elif len(cities) == 1:
+                },
+                {
+                    'title': 'Нет',
+                    'hide': True
 
-        res['response']['text'] = 'Этот город в стране - ' + get_geo_info(cities[0], 'country')
-
-    elif len(cities) == 2:
-
-        distance = get_distance(get_geo_info(cities[0], 'coordinates'),
-                                get_geo_info(cities[1], 'coordinates'))
-        res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
+                }
+            ]
 
     else:
 
-        res['response']['text'] = 'Слишком много городов!'
+        if not sessionStorage[user_id]['game_started']:
+
+            if 'да' in req['request']['nlu']['tokens']:
+
+                if len(sessionStorage[user_id]['guessed_cities']) == 3:
+
+                    res['response']['text'] = 'Ты отгадал все города!'
+                    res['end_session'] = True
+
+                else:
+
+                    sessionStorage[user_id]['game_started'] = True
+                    sessionStorage[user_id]['attempt'] = 1
+                    play_game(res, req)
+
+            elif 'нет' in req['request']['nlu']['tokens']:
+                res['response']['text'] = 'Ну и ладно!'
+                res['end_session'] = True
+            elif "Покажи город на карте" == req['request']['original_utterance']:
+                res['response']['text'] = 'Показала. Сыграем еще? '
+            else:
+                res['response']['text'] = 'Не понял ответа! Так да или нет?'
+                res['response']['buttons'] = [
+                    {
+                        'title': 'Да',
+                        'hide': True
+
+                    },
+                    {
+                        'title': 'Нет',
+                        'hide': True
+
+                    }
+                ]
+
+        else:
+
+            play_game(res, req)
 
 
-def get_cities(req):
-    cities = []
+def play_game(res, req):
+    user_id = req['session']['user_id']
+    attempt = sessionStorage[user_id]['attempt']
 
+    if attempt == 1:
+
+        city = list(cities.keys())[random.randint(0, 2)]
+
+        while (city in sessionStorage[user_id]['guessed_cities']):
+            city = list(cities.keys())[random.randint(0, 2)]
+
+        sessionStorage[user_id]['city'] = city
+
+        res['response']['card'] = {}
+        res['response']['text'] = 'Что это за город?'
+        res['response']['card']['type'] = 'BigImage'
+        res['response']['card']['title'] = 'Что это за город?'
+        res['response']['card']['image_id'] = cities[city][attempt - 1]
+
+    else:
+
+        city = sessionStorage[user_id]['city']
+
+        if get_city(req) == city:
+
+            res['response']['text'] = 'Правильно! Сыграем еще?'
+
+            res['response']['buttons'] = [
+                {
+                    "title": "Да",
+                    "hide": True
+                },
+                {
+                    "title": "Нет",
+                    "hide": True
+                },
+                {
+                    "title": "Покажи город на карте",
+                    "url": "https://yandex.ru/maps/?mode=search&text=%s" % (city),
+                    "hide": True
+                }
+            ]
+
+            sessionStorage[user_id]['guessed_cities'].append(city)
+            sessionStorage[user_id]['game_started'] = False
+            return
+
+        else:
+
+            res['response']['text'] = 'Неправильно'
+            if attempt == 3:
+                res['response']['text'] = 'Вы пытались. Это ' + city.title() + '. Сыграем еще?'
+                sessionStorage[user_id]['game_started'] = False
+                sessionStorage[user_id]['guessed_cities'].append(city)
+                return
+            else:
+                res['response']['card'] = {}
+                res['response']['card']['type'] = 'BigImage'
+                res['response']['card']['title'] = 'Неправильно. Вот тебе дополнительное фото'
+                res['response']['card']['image_id'] = cities[city][attempt - 1]
+
+    sessionStorage[user_id]['attempt'] += 1
+
+
+def get_city(req):
     for entity in req['request']['nlu']['entities']:
 
         if entity['type'] == 'YANDEX.GEO':
 
             if 'city' in entity['value'].keys():
-                cities.append(entity['value']['city'])
+                return entity['value']['city']
+            else:
+                return None
 
-    return cities
+    return None
 
+
+def get_first_name(req):
+    for entity in req['request']['nlu']['entities']:
+
+        if entity['type'] == 'YANDEX.FIO':
+
+            if 'first_name' in entity['value'].keys():
+                return entity['value']['first_name']
+            else:
+                return None
+    return None
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
