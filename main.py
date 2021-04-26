ffe76dc69e26707a02f1f48f78beeb2b7a3b9958
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-26 17:04:20 +0500

    убрал ngrok

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
-from flask_ngrok import run_with_ngrok
 import logging
 import json
 import random
 
 app = Flask(__name__)
-run_with_ngrok(app)
 logging.basicConfig(level=logging.INFO)
 
 cities = {
