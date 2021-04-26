51ac33aab8818b00b74fc13fbbcb2c559b40ea79
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-26 19:49:39 +0500

    переводчик

--- main.py
+++ main.py
@@ -90,81 +90,39 @@ def main():
 
 
 def handle_dialog(res, req):
-    user_id = req['session']['user_id']
-
     if req['session']['new']:
-        res['response']['text'] = 'Привет! Назови свое имя!'
-        names[user_id] = {
-            'name': None
-        }
+        res['response']['text'] = 'Привет! Я умею переводить! Напиши: "Переведи ' \
+                                  '%слово или фразу, которое надо перевести%"'
 
         return
 
-    first_name = get_first_name(req)
+    if req['request']['nlu']['tokens'][0].lower() == 'переведи':
+        mas = req['request']['nlu']['tokens']
+        if 'слово' in mas:
+            mas.remove('слово')
 
-    if names[user_id]['name'] is None:
+        if 'фразу' in mas:
+            print(1)
+            mas.remove('фразу')
 
-        if first_name is None:
-            res['response']['text'] = 'Не раслышала имя. Повтори!'
-        else:
-            names[user_id]['name'] = first_name
-            res['response']['text'] = 'Приятно познакомиться, ' + first_name.title() + '. Я Алиса. ' \
-                                                                                       'Я могу сказать в какой ' \
-                                                                                       'стране город или сказать ' \
-                                                                                       'расстояние между городами!'
-
+        res['response']['text'] = translate(" ".join(mas[1:]))
     else:
-
-        cities = get_cities(req)
-
-        if len(cities) == 0:
-
-            res['response']['text'] = names[user_id][
-                                          'name'].title() + ', ты не написал название не одного города!'
-
-        elif len(cities) == 1:
-
-            res['response']['text'] = names[user_id]['name'].title() + ', этот город в стране - ' \
-                                                                       '' + get_geo_info(cities[0],
-                                                                                         'country')
-
-        elif len(cities) == 2:
-
-            distance = get_distance(get_geo_info(cities[0], 'coordinates'),
-                                    get_geo_info(cities[1], 'coordinates'))
-            res['response']['text'] = names[user_id]['name'].title() + ', расстояние между этими ' \
-                                                                       'городами: ' + str(
-                round(distance)) + ' км.'
-
-        else:
-
-            res['response']['text'] = names[user_id]['name'].title() + ', слишком много городов!'
+        res['response']['text'] = 'Я тебя не поняла. Напиши: "Переведи ' \
+                                  '%слово или фразу, которое надо перевести%"'
 
 
-def get_cities(req):
-    cities = []
+def translate(text):
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-    for entity in req['request']['nlu']['entities']:
+    parameters = {
+        "key": "trnsl.1.1.20210425T142939Z.70e879d43751cd42.8c41b6064daa44da3bbffa270ffc8012644885d3",
+        "text": text,
+        "lang": "ru-en"
+    }
 
-        if entity['type'] == 'YANDEX.GEO':
+    response = requests.get(url, parameters).json()
 
-            if 'city' in entity['value'].keys():
-                cities.append(entity['value']['city'])
-
-    return cities
-
-
-def get_first_name(req):
-    for entity in req['request']['nlu']['entities']:
-
-        if entity['type'] == 'YANDEX.FIO':
-
-            if 'first_name' in entity['value'].keys():
-                return entity['value']['first_name']
-            else:
-                return None
-    return None
-
+    return response['text'][0]
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
