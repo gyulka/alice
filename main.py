a9d0ce4e08064b590c57b944a48d65fa8ff3cf97
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-26 17:09:08 +0500

    ещё подправил после теста postman'ом

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import logging
 import json
@@ -172,4 +173,5 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
